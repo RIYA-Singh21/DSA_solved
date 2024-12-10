19926d71097f42aab82d4ff71109c8522d906973
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2024-12-11 03:41:17 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (21.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -8,12 +8,24 @@ public:
             int notpick=dpfind(n-1,nums,dp);
         return dp[n]=max(pick,notpick);
     }
-    int rob(vector<int>& nums) {
-        int n =nums.size();
-        vector<int>dp(n,-1);
-        return dpfind(n-1,nums,dp);
-        
-        
+    int dp(int n,vector<int>&nums,int prev1,int prev2){
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(n>1)pick+=prev2;
+            int notpick=prev1;
+            int curr=max(pick,notpick);
+            prev2=prev1;
+            prev1=curr;
         }
-    
+        return prev1;
+        
+        
+    }
+    int rob(vector<int>& nums) {
+        int prev1=nums[0];
+        int prev2=0;
+        int n=nums.size();
+        return dp(n,nums,prev1,prev2);
+        
+    }
 };
