7aaea90766f44846145ef2c0254444a15de74daa
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-02-05 16:19:44 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.5%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,27 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void traversal(TreeNode*root,vector<int>&ans){
-        if(root){
+    void traversal(TreeNode* root, vector<int>& ans) {
+        if (root) {
             ans.push_back(root->val);
-            if(root->left)
-                traversal(root->left,ans);
-            if(root->right)
-                traversal(root->right,ans);
+            if (root->left)
+                traversal(root->left, ans);
+            if (root->right)
+                traversal(root->right, ans);
         }
         return;
     }
+    //iterative approach
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(root==NULL) return {} ;
-        traversal(root,ans);
+        vector<int> ans;
+        //if (root == NULL)
+          //  return {};
+        //traversal(root, ans);
+    //Iterative approach
+    if(root==NULL)return ans;
+    stack<TreeNode*>st;
+    st.push(root);
+    while(!st.empty() ){
+        root=st.top();
+        st.pop();
+        ans.push_back(root->val);
+        if(root->right) st.push(root->right);
+        if(root->left) st.push(root->left);
+    }
         return ans;
-        
-        
     }
 };
