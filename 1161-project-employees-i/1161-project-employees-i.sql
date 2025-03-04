4fb28bdd391d66395ed693e3b2771dd41aa8683f
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-03-04 19:22:03 +0530

    Time: 741 ms (85.86%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select p.project_id, ROUND(AVG(e.experience_years),2) as average_years from project p LEFT JOIN employee e on
 p.employee_id=e.employee_id group by p.project_id;
