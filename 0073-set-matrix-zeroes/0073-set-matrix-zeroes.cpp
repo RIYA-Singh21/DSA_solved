af5ccba4624acf4533e5eaae1742c0039cfa488e
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-03-17 05:07:09 +0530

    Time: 0 ms (100%), Space: 18.4 MB (95.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-//better sol: o(n^2)
+//optimal O(1)
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int>row(n,0);
-        vector<int>col(m,0);
+        int colZero=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    //mark the col
+                    matrix[i][0]=0;
+                    //mark the row
+                    if(j!=0)
+                        matrix[0][j]=0;
+                    else 
+                        colZero=0;
                 }
             }
         }
-        for(int i=0;i<n;i++){
+        //start from the 1st row and 1st col
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                //check whether the element is 0 or not 
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        //check for the first row (based on the first element of the matrix)
+        if(matrix[0][0]==0){
             for(int j=0;j<m;j++){
-                if(row[i] || col[j]){
-                    matrix[i][j]=0;
-                }
+                matrix[0][j]=0;
             }
         }
+        //checl the first col based ont he colZero element
+        if(colZero==0){
+            for(int i=0;i<n;i++) matrix[i][0]=0;
+        }
+
     }
 };
