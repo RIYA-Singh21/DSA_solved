c2f9f44c4c40bd9f43516a3dadd19b7449a2d292
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-03-18 04:55:23 +0530

    Time: 49 ms (56.63%), Space: 51.1 MB (18.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,7 +3,7 @@ public:
 //optimal soln : O(n) (prefix sum approach)
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mpp;
-        //initialize 0,1
+        //initialize 0,1 in the map
         mpp[0]=1;
         int prefixsum=0,cnt=0;
         for(int i=0;i<nums.size();i++){
