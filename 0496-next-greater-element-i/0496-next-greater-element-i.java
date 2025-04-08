9755b5668f552fdba10be1a25ef05234c003cbcd
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-04-09 05:28:55 +0530

    Time: 5 ms (18.09%), Space: 43.7 MB (65.9%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,30 @@
 class Solution {
-    public int ngreater(int num, int[]arr){
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==num){
-                for(int j=i+1;j<arr.length;j++){
-                    if(arr[j]>num){
-                        return arr[j];
-                    }
+    //monotonic stack 
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n=nums2.length;
+        int[]nge=new int[n];
+        Stack<Integer>st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=nums2[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) nge[i]=-1;
+            else nge[i]=st.peek();
+            st.push(nums2[i]);
+        }
+        int m=nums1.length;
+        int[]res=new int[m];
+        //loop nums2 to find the nums1 element
+        //from nums1
+        for(int i=0;i<m;i++){
+            //search in nums2
+            for(int j=0;j<n;j++){
+                if(nums1[i]==nums2[j]){
+                    res[i]=nge[j];
+                    break;
                 }
-                //if there's no greater element than num 
-                return -1;
             }
         }
-        return -1;
-    }
-    //brute force 
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int[]res=new int[n];
-        for(int i=0;i<n;i++){
-            res[i]=ngreater(nums1[i],nums2);
-        }
         return res;
-
     }
 }
