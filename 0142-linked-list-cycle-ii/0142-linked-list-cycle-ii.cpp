08921489f23b60999aefd9964db4eaf8bccb280c
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-07-22 16:48:42 +0530

    Time: 5 ms (83.93%), Space: 11.4 MB (25.03%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,16 +8,21 @@
  */
 class Solution {
 public:
-//using hashmap
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode *,bool>visited;
-        ListNode*node=head;
-        while(node!=NULL){
-            if(visited[node])return node;
-            visited[node]=true;
-            node=node->next;
+        ListNode*fast=head;
+        ListNode*slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
         }
         return NULL;
-
     }
 };
