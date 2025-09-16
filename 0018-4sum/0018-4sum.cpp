59ca0cca1a6fa43327b28a4f0ddcb8e252ae0193
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-09-16 17:25:22 +0530

    Time: 23 ms (54.19%), Space: 17.2 MB (98.29%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-//better approach
+//optimal approach- keep i,j const  move k and l as 2 ptrs
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        //set for unique val only
-        set<vector<int>>st;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n;i++){
+            //move till you find a diff element than the last one
+            if(i>0 && nums[i]==nums[i-1])continue;
             for(int j=i+1;j<n;j++){
-                //a hashset to store the elements from j->k
-                set<long long>hashset;
-                for(int k=j+1;k<n;k++){
-                    //removing the 4th loop
-                    long long sum=nums[i]+nums[j];
+                //if j is not the first ele 
+                if(j!=i+1 && nums[j]==nums[j-1])continue;
+                int k=j+1,l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                    sum+=nums[j];
                     sum+=nums[k];
-                    long long fourth=target-sum;
-                        if(hashset.find(fourth)!=hashset.end()){
-                            vector<int>temp={nums[i],nums[j],nums[k],(int)fourth};
-                            sort(temp.begin(),temp.end());
-                            st.insert(temp);
-                        }
-                        hashset.insert(nums[k]);
+                    sum+=nums[l];
+                    if(target==sum){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;l--;
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l]==nums[l+1])l--;
                     }
+                    else if(target<sum) l--;
+                    else k++;
+                
                 }
             }
-        vector<vector<int>>res(st.begin(),st.end());
-        return res;
+        }
+        return ans;
     }
 };
