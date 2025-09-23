bfb6e2c6ddaeba8d218045335739a5f475b8c00f
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-09-23 22:20:23 +0530

    Time: 7 ms (67.9%), Space: 11 MB (80.29%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-//2ptr and sliding window
+//sliding window 2 ptr
     int numberOfSubstrings(string s) {
+        int n=s.size();
+        //lastseen array
+        int hash[3]={-1,-1,-1};
+        int r=0;
         int cnt=0;
-        vector<int>lastseen(3,-1);
-        for(int i=0;i<s.size();i++){
-             lastseen[s[i]-'a']=i;
-            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1 ){
-                int minval=min({lastseen[0],lastseen[1],lastseen[2]});
-                cnt=cnt+(minval+1);
+        while(r<n){
+            hash[s[r]-'a']=r;
+            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                cnt+=(1+min(hash[0],min(hash[1],hash[2])));
             }
+            r++;
         }
-        return cnt;
+    return cnt;
     }
 };
