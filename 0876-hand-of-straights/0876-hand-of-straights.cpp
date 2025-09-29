bd8392276b6d0e4d3fb927503c0f23a784cf47dd
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-09-29 13:03:15 +0530

    Time: 51 ms (13.7%), Space: 32.2 MB (74.37%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+//using ordered map
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        //if the number of ele are not divisible by grpSize then ret false
+        if(hand.size()%groupSize!=0)
+            return false;
+        map<int,int>mp;
+        for(int i=0;i<hand.size();i++){
+            mp[hand[i]]++;
+        }
+        while(!mp.empty()){
+            //store the first ele of the group
+            int curr=mp.begin()->first;//first:ele second : freq
+            //no. of ele in the group = groupSize so loop through the groupSize
+            for(int i=0;i<groupSize;i++){
+                //if ele is not present 
+                if(mp[curr+i]==0)
+                    return false;
+                mp[curr+i]--;
+                //if the element's freq ==0 remove it from the map
+                if(mp[curr+i]<1)
+                    mp.erase(curr+i);
+            }
+
+        }
+        return true;
+   }
+
+};
