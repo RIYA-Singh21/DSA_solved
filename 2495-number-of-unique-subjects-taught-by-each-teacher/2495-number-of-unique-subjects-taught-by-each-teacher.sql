ad0120442fc46d4a2c73e8489f34d60331b46753
Riya singh <129723734+RIYA-Singh21@users.noreply.github.com>
2025-10-07 18:20:57 +0530

    Time: 479 ms (78.66%), Space: 0B (100%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select t1.teacher_id,count(distinct t1.subject_id) as cnt from Teacher t1 left join Teacher t2 on t1.teacher_id =t2.teacher_id group by t1.teacher_id
+select t1.teacher_id,count(distinct t1.subject_id) as cnt from Teacher t1 group by t1.teacher_id
